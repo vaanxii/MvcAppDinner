51701dd5551327f1f1f3d1928378319f00dfd6e7
Ryan Glenn Resco <eronkid@gmail.com>
2012-11-02 17:57:04 +0800

    Adding Person Model

---
+++ Person.Models/ViewModels/PersonViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Person.Models.ViewModels
+{
+    public class PersonViewModel
+    {
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
